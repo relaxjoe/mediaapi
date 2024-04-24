25d5e5a354955092dfdc70b3c108133ddb3b19fc
Joe <joeyndiebel@gmail.com>
2024-04-23 19:43:08 -0500

    headstart

---
+++ server.js
@@ -0,0 +1,18 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.listen(process.env.PORT || 3001, () => console.log(`🌍 Connected on localhost:${process.env.PORT || 3001}`));
